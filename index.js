43b34c23d4eb6adfbb271f546bad75909d9c4165
jackrobert0220 <jackrobert0220@gmail.com>
2022-06-30 00:54:22 -0700

    concatonated javascript

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  renderLeads()
+  inputEl.value = "";
+  renderLeads();
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `
+      <li>
+        <a target='_blank' href='${myLeads[i]}'>
+          ${myLeads[i]}
+        </a>
+      </li>
+      `;
     console.log(listItems);
   }
   ulEl.innerHTML = listItems;
